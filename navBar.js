9557b90c19e39381d7a61fbe84ac7d2d82e0fa9f
Amanda Grimm <amandagrimm95@outlook.com>
2022-01-28 11:00:48 -0500

    Responsive adjustments

---
+++ navBar.js
@@ -0,0 +1,11 @@
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x = document.getElementById("navLinks");
+    x.classList.toggle('show')
+    /*if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }*/
+  }
+  
